4d4b20dea6acc8ba66fa13c024b00a7b031982b7
shaown <smshaown65@gmail.com>
2021-06-25 16:55:39 +0600

    ekono baki ache

---
+++ app.js
@@ -0,0 +1,9 @@
+// Init Github;
+const github = new Github;
+
+const searchUser = document.getElementById('search-user');
+
+searchUser.addEventListener('keyup', (e) => {
+    if.24.30
+    github.getUser(searchText);
+})
---
+++ github.js
@@ -0,0 +1,9 @@
+class Github {
+    constructor(){
+        this.client_id = '';
+        this.client_secret = '';
+    }
+    getUser(user){
+        console.log(user);
+    }
+}
