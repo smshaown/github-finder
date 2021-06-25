30a1404b46b4e2dcf6a750b8c49e47e48a35bbf7
shaown <smshaown65@gmail.com>
2021-06-26 05:48:35 +0600

    git profile done

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ searchUser.addEventListener('keyup', (e) => {
     const searchText = e.target.value;
 
     if(searchText !== ''){
-        github.getUser(searchText);
+        github.getUser(searchText).then(data => console.log(data));
     }else{
         
     }
--- github.js
+++ github.js
@@ -1,9 +1,14 @@
 class Github {
     constructor(){
-        this.client_id = '';
-        this.client_secret = '';
+        this.client_id = '2677e2449c8ebdc9bbc3';
+        this.client_secret = '66284c842373e066414aae5fe123e271015cc2aa';
     }
-    getUser(user){
-        console.log(user);
+   async getUser(user){
+        const profileResponse = await fetch( `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+        const profile = await profileResponse.json();
+
+        return{
+            profile
+        }
     }
 }
