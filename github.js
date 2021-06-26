7d4383cdc710c11052dec7e05e9846c9beff64bf
shaown <smshaown65@gmail.com>
2021-06-26 07:16:53 +0600

    github finder project codeing the success

--- github.js
+++ github.js
@@ -1,14 +1,21 @@
 class Github {
     constructor(){
-        this.client_id = '2677e2449c8ebdc9bbc3';
-        this.client_secret = '66284c842373e066414aae5fe123e271015cc2aa';
+        this.client_id = '65b44d46d520be1f19c7';
+        this.client_secret = '7287ef205413001a79b30f0fbcc04416153ef797';
+        this.repos_count   = 5;
+        this.repos_sort = 'created: asc'
     }
-   async getUser(user){
-        const profileResponse = await fetch( `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    async getUser(user){
+        const profileResponse = await  fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
         const profile = await profileResponse.json();
 
-        return{
-            profile
+        const reposResponse = await  fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+        const repos = await reposResponse.json();
+
+        return {
+            profile,
+            repos
         }
     }
 }
